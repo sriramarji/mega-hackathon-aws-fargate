a030dab7139689005984deb75bf1e4885c66f950
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-07-19 11:59:27 +0530

    updated backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hcl-mega-hackathon"
+    bucket = "hcl-mega-hackathon-bhaskar"
     key    = "aws-fargate/terraform.tfstate"
     region = "us-west-1" 
 }
